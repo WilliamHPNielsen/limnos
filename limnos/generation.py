81a4b6fd3b6a5b400fdc57978f6e83edc6e434a0
William Nielsen <whpn@mailbox.org>
2021-04-02 16:51:41 +0200

    Fix intersection check to make tests pass

--- limnos/generation.py
+++ limnos/generation.py
@@ -29,12 +29,44 @@ def _wall_intersects_route(route: Route, wall: Wall) -> bool:
     points_in_route = (route_p0 in route) and (route_p1 in route)
     if points_in_route:
         p0_ind = route.index(route_p0)
-        points_neighbours = (route[p0_ind + 1] == route_p1 or
-                             route[p0_ind - 1] == route_p1)
+
+        # special case: the end of the route
+        if p0_ind == len(route) - 1:
+            points_neighbours = route[p0_ind - 1] == route_p1
+        else:
+            points_neighbours = (route[p0_ind + 1] == route_p1 or
+                                 route[p0_ind - 1] == route_p1)
     else:
         points_neighbours = False
 
     return points_in_route and points_neighbours
+
+
+def _wall_forms_loop(new_wall: Wall, old_walls: Walls) -> bool:
+    """
+    Check whether adding the new_wall to the old_walls would form
+    a closed wall-loop
+    """
+
+    p1 = new_wall[0]
+    p2 = new_wall[1]
+
+    found_p1 = False
+    found_p2 = False
+
+    # TODO: This condition is too strict, sadly, as it prevents
+    # ANY gap from being filled, not just loop-forming ones
+    for wall in old_walls:
+        if not(found_p1):
+            if p1 in wall:
+                found_p1 = True
+        if not(found_p2):
+            if p2 in wall:
+                found_p2 = True
+        if found_p1 and found_p2:
+            break
+
+    return found_p1 and found_p2
 
 
 def add_outer_walls_to_maze(maze: Maze) -> Maze:
@@ -69,8 +101,6 @@ def add_random_wall_to_maze(maze: Maze) -> Maze:
     Add a random (inner) wall to a maze
     """
 
-    # need additional "no wall loops" rule
-
     route: Route = maze[0]
     walls: Walls = maze[1]
 
@@ -79,9 +109,9 @@ def add_random_wall_to_maze(maze: Maze) -> Maze:
     x1 = route[-1][0] - 1
     y1 = route[-1][1] - 1
 
-    intersection = True
+    found_good_wall = False
 
-    while intersection:
+    while not(found_good_wall):
 
         x = 2 * randint(x0 / 2, x1 / 2)
         y = 2 * randint(y0 / 2, y1 / 2)
@@ -93,10 +123,14 @@ def add_random_wall_to_maze(maze: Maze) -> Maze:
 
         wall: Wall = ((x, y), add_points((x, y), step))
 
-        if wall in walls:
-            continue
+        reasons_to_retry = [wall in walls,
+                            _wall_forms_loop(wall, walls),
+                            _wall_intersects_route(route, wall)]
 
-        intersection = _wall_intersects_route(route, wall)
+        if any(reasons_to_retry):
+            pass
+        else:
+            found_good_wall = True
 
     new_walls = walls.copy()
     new_walls.append(wall)
