8e2401a5c7b817f901cd63c25be9bad19b7a1439
William Nielsen <whpn@mailbox.org>
2021-04-08 11:00:02 +0200

    Add color coding to trails plot

--- limnos/visualisation.py
+++ limnos/visualisation.py
@@ -5,7 +5,25 @@ from .types import Route, Routes, Maze, Trails
 
 import matplotlib.pyplot as plt
 
+ColorCode = str
+
 ROUTE_COLOR = "#b0aaa3"
+ROUTE_COLORS: list[ColorCode] = [
+    "#89fb89",
+    "#feff8f",
+    "#ffb162",
+    "#07b8b4",
+    "#a33800"]
+
+
+def color_picker(colors: list[ColorCode]) -> ColorCode:
+    """
+    Generator that yields colors
+    """
+    n = 0
+    while True:
+        yield colors[n]
+        n = (n + 1) % len(colors)
 
 
 def plot_route(route: Route) -> None:
@@ -34,19 +52,26 @@ def plot_routes(routes: Routes) -> None:
     plt.show()
 
 
-def plot_trails(trails: Trails) -> None:
+def plot_trails(trails: Trails, color_coding: bool=False) -> None:
     """
     Plot a collection of trails
     """
+
+    if color_coding:
+        cp = color_picker(ROUTE_COLORS)
+    else:
+        cp = color_picker([ROUTE_COLOR])
 
     fig, ax = plt.subplots()
 
     for route in trails.all_routes():
 
+        route_color = next(cp)
+
         for this_point, next_point in zip(route[:-1], route[1:]):
             xs = (this_point[0], next_point[0])
             ys = (this_point[1], next_point[1])
-            ax.plot(xs, ys, '-o', color=ROUTE_COLOR)
+            ax.plot(xs, ys, '-o', color=route_color)
 
     plt.show()
 
