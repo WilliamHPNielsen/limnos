1f752c8cad3a7028a04188d7859023d5ef33e55c
Petter Becker-Jostes <petterbj@outlook.dk>
2021-05-12 12:32:05 +0200

    Add pseudocode for step 3 of random_base_solution and its test.

--- limnos/tests/test_generation.py
+++ limnos/tests/test_generation.py
@@ -3,7 +3,7 @@ Test the generation functions
 """
 import pytest
 
-from limnos.generation import _wall_intersects_route
+from limnos.generation import _wall_intersects_route, random_base_solution
 
 
 ROUTE = [(1, 1), (1, 3), (3, 3), (5, 3), (5, 5)]
@@ -16,3 +16,12 @@ intersects = [False, True, True]
 @pytest.mark.parametrize("wall, intersects", list(zip(walls, intersects)))
 def test_intersection_check(wall, intersects):
     assert _wall_intersects_route(ROUTE, wall) == intersects
+
+def test_random_base_solution:
+    # Test should check the following:
+    # A) That upper right corner is always the last step (and thus also that
+    # it is actually there)
+    # B) That there are no points outside of the maze
+    # C) It might be a good idea that this test constists of creating several
+    # actual mazes and then run the tests on these.
+    pass
